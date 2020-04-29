ecf9a6e20074873b2b0ab0fdd3e7403cddaf2973
Sami Rouhe <rouhesami@gmail.com>
2020-04-29 14:02:59 +0300

    update

--- snowflake/snowflakectl.py
+++ snowflake/snowflakectl.py
@@ -2,7 +2,7 @@ from time import sleep
 
 def main():
     while 1:
-        print('Running snowflakectl')
+        print('Running snowflakectl, 1.0.1')
         sleep(2)
 
 if __name__ == '__main__':
